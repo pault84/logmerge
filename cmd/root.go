07c514b89ddc7fedefad1a901421bd62daee12f6
pault84 <paul@portworx.com>
2021-11-01 20:52:34 -0700

    remove portworx from text
    Signed-off-by: pault84 <paul@portworx.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,14 @@
 package cmd
 
 import (
-
 	"github.com/spf13/cobra"
 )
 
 var (
 	rootCmd = &cobra.Command{
 		Use:   "logmerge",
-		Short: "A Logmerging tool for portworx",
-		Long: `Logmerge is a tool that allows us to merge multiple log files based on date.`,
+		Short: "A Logmerging tool that merges logfiles with parseable datetime formats in their lines.",
+		Long:  `Logmerge is a tool that allows us to merge multiple log files based on date.`,
 	}
 )
 
